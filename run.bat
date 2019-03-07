306690d315b23cbcfa7f945fcdd7b3ffa0abd7f6
Sergei Trishkin <43265300+grdvsng@users.noreply.github.com>
2019-03-07 19:30:24 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,16 @@
+title Fix WebRec
 @echo off
 @cd/d "%~dp0"
 
-ûåôêå ÝREGEDIT /S main.reg
-echo %cd%\webrec
-
-xcopy "%cd%\webrec" "C:\Program Files\webrec\" /s /y
-
+call :def_main
 pause
+exit
 
+:def_main
+	REGEDIT /S main.reg
+	echo %cd%\webrec
+
+	xcopy "%cd%\bin" "C:\Program Files\webrec\" /s /y
+	cls
+	
+	if errorlevel 1 ( echo Please start as Admin. ) else ( echo Done! )
